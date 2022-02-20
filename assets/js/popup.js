b8247119871077292108d5891874a6eacd0817b1
shaanfye <IndianBoiSwag14$$>
2022-02-19 19:17:07 -0500

    commit one

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -2,13 +2,24 @@ const text = document.getElementById( 'notify-text' );
 const notify = document.getElementById( 'notify-button' );
 const reset = document.getElementById( 'notify-reset' );
 const counter = document.getElementById( 'notify-count' );
+const shaan = document.getElementById( 'shaan-button' );
+let curURL; 
+let curVal = 0;
+var the_array = [];
 
 chrome.storage.local.get( ['notifyCount'], data => {
-	let value = data.notifyCount || 0;
-	counter.innerHTML = value;
+	//curVal = data.notifyCount || 0; // gets the value or sets to 0
+	//curVal = 0;
+	//counter.innerHTML = curVal;
 } );
 
 chrome.storage.onChanged.addListener( ( changes, namespace ) => {
+	chrome.storage.local.get({'links': []}, function(obj) {
+		console.log(obj.links);
+		the_array = obj.links;
+		// need to make sure it grabs ddata on startup
+	})
+	
 	if ( changes.notifyCount ) {
 		let value = changes.notifyCount.newValue || 0;
 		counter.innerHTML = value;
@@ -17,7 +28,8 @@ chrome.storage.onChanged.addListener( ( changes, namespace ) => {
 
 reset.addEventListener( 'click', () => {
 	chrome.storage.local.clear();
-	text.value = '';
+	curVal = 0;
+	the_array = [];
 } );
 
 notify.addEventListener( 'click', () => {
@@ -26,3 +38,23 @@ notify.addEventListener( 'click', () => {
 		message: text.value
 	});
 } );
+
+
+shaan.addEventListener( 'click', () => {
+	curVal +=1;
+	chrome.storage.local.set({"notifyCount": curVal}, function() {
+		console.log(`Value of your URL is set to ` + curVal);
+	
+		chrome.tabs.query({active: true, lastFocusedWindow: true}, async tabs => {
+			curURL = await tabs[0].url;
+			// need to figure out how to wait for this to occur
+			the_array.push(curURL); // inline push
+			chrome.storage.local.set({links: the_array}, function() {
+				//console.log(`Value is set to ` + curURL);
+			});
+		})		
+		
+	});
+	
+} );
+
--- background.js
+++ background.js
@@ -4,6 +4,29 @@ chrome.runtime.onMessage.addListener( data => {
 	}
 });
 
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+	console.log(tabId);
+	console.log(changeInfo);
+	console.log(tab);
+	console.log(tab.url);
+})
+
+chrome.storage.onChanged.addListener( function (changes, namespace) {
+	
+	console.log(changes.notify.oldValue);
+	for(let [key, {oldValue, newValue}] of Object.entries(changes)){
+		console.log(`the values key ` + key);
+		console.log(`the values are ${oldValue} and ${newValue}`);
+	}
+	
+	console.log(`the namespaces are ` + namespace);
+	chrome.storage.local.get(function(result){
+		console.log(result);
+	})
+})
+
+
+
 chrome.runtime.onInstalled.addListener( () => {
 	chrome.contextMenus.create({
 		id: 'notify',
@@ -12,25 +35,26 @@ chrome.runtime.onInstalled.addListener( () => {
 	});
 });
 
-chrome.contextMenus.onClicked.addListener( ( info, tab ) => {
-	if ( 'notify' === info.menuItemId ) {
-		notify( info.selectionText );
-	}
-} );
+// chrome.contextMenus.onClicked.addListener( ( info, tab ) => {
+// 	if ( 'notify' === info.menuItemId ) {
+// 		notify( info.selectionText );
+// 	}
+// } );
 
-const notify = message => {
-	chrome.storage.local.get( ['notifyCount'], data => {
-		let value = data.notifyCount || 0;
-		chrome.storage.local.set({ 'notifyCount': Number( value ) + 1 });
-	} );
+// const notify = message => {
+// 	chrome.storage.local.get( ['notifyCount'], data => {
+// 		let value = data.notifyCount || 0;
+// 		chrome.storage.local.set({ 'notifyCount': Number( value ) + 1 });
+// 	} );
 
-	return chrome.notifications.create(
-		'',
-		{
-			type: 'basic',
-			title: 'Notify!',
-			message: message || 'Notify!',
-			iconUrl: './assets/icons/128.png',
-		}
-	);
-};
+// 	return chrome.notifications.create(
+// 		'',
+// 		{
+// 			type: 'basic',
+// 			title: 'Notify!',
+// 			message: message || 'Notify!',
+// 			iconUrl: './assets/icons/128.png',
+// 		}
+// 	);
+// };
+
